f063960a04520f71c39e5f8e2ae22627c55b1f0f
mre55 <misganawmihreteab>
2022-06-21 13:13:48 +0300

    Add MUI form to the page

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,98 @@
-import React from 'react'
+import React, { useState } from "react";
+import FileBase from "react-file-base64";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 import useStyles from "./styles";
 const Form = () => {
-  const classes = useStyles();
-  return (
-    <h1>Form</h1>
-  )
-}
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectFile: "",
+  });
 
-export default Form
+  const classes = useStyles();
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const clear = () => {};
+
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          label="Creator"
+          variant="outlined"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData({ ...postData, creator: e.target.value })
+          }
+        />
+        <TextField
+          name="title"
+          label="Title"
+          variant="outlined"
+          fullWidth
+          value={postData.title}
+          onChange={(e) => setPostData({ ...postData, title: e.target.value })}
+        />
+        <TextField
+          name="message"
+          label="Message"
+          variant="outlined"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData({ ...postData, message: e.target.value })
+          }
+        />
+        <TextField
+          name="tags"
+          label="Tags"
+          variant="outlined"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            mutiple={false}
+            onDone={({ base64 }) =>
+              setPostData({ ...postData, selectFile: base64 })
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          type="submit"
+          color="primary"
+          size="large"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          size="small"
+          fullWidth
+          onClick={clear}
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
+};
+
+export default Form;
