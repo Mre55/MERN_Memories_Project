0d18a8d45a567543703673a2ec4662223d8321f2
mre55 <misganawmihreteab>
2022-06-21 13:38:07 +0300

    Map in posts which are coming from store

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post'
-
 
 import useStyles from "./styles";
 const Posts = () => {
   const posts = useSelector(state => state.posts);
   const classes = useStyles();
 
-  console.log(posts);
-
   return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
-  )
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems='strech' spacing={3} >
+        {
+          posts.map(post => (
+            <Grid item xs={12} sm={6} key={post._id}>
+              <Post post={post} />
+            </Grid>
+          ))
+        }
+
+      </Grid>
+  ));
 }
 
 export default Posts
