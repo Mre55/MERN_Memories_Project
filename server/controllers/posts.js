ea6cd9f30814ecf837ae8502433224871da977bb
mre55 <misganawmihreteab>
2022-06-21 23:05:26 +0300

    Add like post method for the backend

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -44,5 +44,15 @@ export const deletePost = async (req, res) => {
   await PostMessage.findByIdAndRemove(_id);
 
   res.json({ message: "Post deleted successfuly" });
+}
 
+export const likePost = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json("No post with that ID");
+
+  const post = await PostMessage.findById(_id);
+  const updatedPost = await postMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1}, { new: true });
+
+  res.json(updatePost);
 }
