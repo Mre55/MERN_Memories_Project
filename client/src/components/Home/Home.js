80911745ff68b7bcebdc361a0c3a8dbf3afe7307
mre55 <misganawmihreteab>
2022-06-22 15:37:20 +0300

    Render Post and Form component in Home component

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Container, Grid, Grow } from "@material-ui/core";
+
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+
+import { getPosts } from "../../actions/posts";
+import useStyles from "./styles";
+
+const Home = () => {
+
+  const [currentId, setCurrentId] = useState(null);
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+  
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          className={classes.mainContainer}
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
