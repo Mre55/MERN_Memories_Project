a191ff9770b646e6a486651dc33633109e0d037f
mre55 <misganawmihreteab>
2022-07-05 23:04:49 +0300

    Wrap the app with google auth

--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import { reducers } from "./reducers";
 import App from "./App";
-import './index.css';
+import "./index.css";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <GoogleOAuthProvider clientId="1056514434052-laaoj48mu0hklc2apvrnk07s6macojvf.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </Provider>,
   document.getElementById("root")
 );
