e56034bdc4e86ed43f8ed8152cc7b61ab931911e
mre55 <misganawmihreteab>
2022-07-13 19:33:10 +0300

    Add signin and signup actions

---
+++ client/src/actions/auth.js
@@ -0,0 +1,20 @@
+import { AUTH } from "../constants/actionTypes";
+import * as api from "../api";
+
+export const signin = (formData, history) => async (dispatch) => {
+  try {
+    // sign in the user
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signup = (formData, history) => async (dispatch) => {
+  try {
+    // sign up the user
+    history.push("/");
+  } catch (error) {
+    console.log(error);
+  }
+};
